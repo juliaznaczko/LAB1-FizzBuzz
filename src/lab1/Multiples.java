d93a7c5c3e243c8a242bfeef6c839c77390c56c7
Lana Tkachuk <lanatkachuk@Lanas-MacBook-Pro-2.local>
2025-09-09 13:59:33 -0400

    Created multiples method to count multiples of a and b under n.

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,18 +2,23 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
+        int count = multiples(1000, 5,3);
+        System.out.println(count);
+    }
+
+    static int multiples(int n, int a, int b) {
         int total = 0;
 
-        for  (int i = 1; i <= 1000; i++) {
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
+        for  (int i = 1; i < n; i++) {
+            boolean divisibleBya = i % a == 0;
+            boolean divisibleByb = i % b == 0;
 
-            if (divisibleBy3) {
+            if (divisibleBya) {
                 total +=1;
-            } else if (divisibleBy5) {
+            } else if (divisibleByb) {
                 total += 1;
             }
         }
-        System.out.println(total);
+        return total;
     }
 }
