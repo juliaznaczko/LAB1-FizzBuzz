ae513415418aa8ae8abe9aae1bcf3d478a35114d
Lana Tkachuk <lanatkachuk@Lanas-MacBook-Pro-2.local>
2025-09-10 10:03:19 -0400

    reduce modified

--- src/lab1/Reduce.java
+++ src/lab1/Reduce.java
@@ -7,16 +7,15 @@ public class Reduce {
     }
 
     public static int reduce(int n) {
-        int i = 100;
         int total = 0;
 
-        while (i > 0) {
-            if (i % 2 == 0) {
-                i /= 2;
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n /= 2;
                 total += 1;
             }
             else {
-                i -= 1;
+                n -= 1;
                 total += 1;
             }
 
